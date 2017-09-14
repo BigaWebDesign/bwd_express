d623ecc4b98f5c4ab110a633f9681e10e092f245
Emir TAŞÇI <programarsivim52@gmail.com>
2017-09-14 19:12:06 +0300

    bwd_yeni

---
+++ server.js
@@ -0,0 +1,38 @@
+var favicon = require('serve-favicon');
+var app = require('express')(),
+	port = process.env.PORT || 3000,
+	morgan = require('morgan');
+
+//conf
+app.use(morgan('prod'));
+
+//route
+app.get('/', function(req, res){
+
+res.sendFile(__dirname + '/index.html');
+});
+
+app.get('/hakkimizda', function(req, res){
+
+	res.sendFile(__dirname+ '/templates/pages/hakkimizda.html');
+
+});
+
+app.use(function(req, res, next) {
+	res.status(404);
+	res.sendFile(__dirname + '/templates/err/404.html');
+  });
+app.use(function(req, res, next) {
+	res.status(500);
+	res.sendFile(__dirname + '/templates/err/404.html');
+});
+
+app.use(favicon(__dirname + '/favicon.png'));
+
+
+
+//server
+app.listen(port, function() {
+	console.log('server başlatıldı! http://localhost:' + port);
+})
+
